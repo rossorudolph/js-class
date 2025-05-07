08c17700109b59d7b4ba4ce8eadad073af4d099f
rossorudolph <rossorudolph@gmail.com>
2025-05-06 20:19:46 -0700

    add event listener

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function updateClickClount() {
 numClicks = numClicks + 1;
 console.log(numClicks);
 }
+
+document.body.addEventListener("click", logClickLocation);
+function logClickLocation(eventData) {
+console.log( "User clicked at " + eventData.clientX + ", " +
+eventData.clientY );
+}
