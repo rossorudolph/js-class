80336954403418159ac8a2ceeae892b7eb26bb36
rossorudolph <rossorudolph@gmail.com>
2025-05-06 20:44:18 -0700

    added gif

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
 console.log("Hello World!");
 
-let numClicks = 0;
-function updateClickClount() {
-numClicks = numClicks + 1;
-console.log(numClicks);
-}
-
-document.body.addEventListener("click", logClickLocation);
-function logClickLocation(eventData) {
-console.log( "User clicked at " + eventData.clientX + ", " +
-eventData.clientY );
-}
